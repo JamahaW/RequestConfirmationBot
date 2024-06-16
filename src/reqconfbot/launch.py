acd69f6b779f520880f1af72a6372e106e94b704
JamahaW <pret2titio1n@gmail.com>
2024-06-17 00:08:00 +0300

    cmds

--- src/reqconfbot/launch.py
+++ src/reqconfbot/launch.py
@@ -1,3 +1,5 @@
+import random
+
 from discord import Intents, Message
 from discord.ext.commands import Bot
 
@@ -17,10 +19,21 @@ def getMessageInfo(msg: Message) -> str:
     return f"{msg.author}: {msg.content}"
 
 
-@bot.event
-async def on_message(ctx: Message):
-    if ctx.author != bot.user:
-        await ctx.reply(getMessageInfo(ctx))
+# @bot.event
+# async def on_message(ctx: Message):
+#     if ctx.author != bot.user:
+#         await ctx.reply(getMessageInfo(ctx))
+#
+
+@bot.command()
+async def rand(ctx, *arg):
+    await ctx.reply(random.randint(0, 100))
+
+
+@bot.command()
+async def sq(ctx, *args):
+    x = int(args[0])
+    await ctx.reply(f"{x}*{x} = {x * x}")
 
 
 if __name__ == '__main__':
